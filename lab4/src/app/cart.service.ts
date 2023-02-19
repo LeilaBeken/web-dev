6afaee294db144bfecc77af0bb973d46d702db4f
LeilaBeken <98761916+LeilaBeken@users.noreply.github.com>
2023-02-19 17:40:10 +0600

    nnnn

--- lab4/src/app/cart.service.ts
+++ lab4/src/app/cart.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Product } from './products';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  constructor(
+    private http : HttpClient
+  ){}
   item : Product[] = [];
 
   addToCart(product: Product){
@@ -18,5 +22,9 @@ export class CartService {
   clearCart(){
     this.item = [];
     return this.item;
+  }
+
+  getShippingPrices(){
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 }
