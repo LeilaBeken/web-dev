b7d40c6e1ff263ba4fbec40f44f319d382b99d29
LeilaBeken <98761916+LeilaBeken@users.noreply.github.com>
2023-02-19 18:47:11 +0600

    nnnn

--- lab4/src/app/cart/cart.component.ts
+++ lab4/src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -8,7 +9,19 @@ import { CartService } from '../cart.service';
 })
 export class CartComponent {
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder
   ){}
   items =this.cartService.getItems();
+
+  checkOutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
+  onSubmit(): void{
+    this.items = this.cartService.clearCart();
+    console.warn('You order has been submitted', this.checkOutForm.value);
+    this.checkOutForm.reset();
+  }
 }
