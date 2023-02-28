71d2f63c9a22bf56e5b869a178682fd4c39ac4ec
LeilaBeken <98761916+LeilaBeken@users.noreply.github.com>
2023-02-28 23:10:01 +0600

    hhhh

---
+++ lab5/kaspi/src/app/product-list/product-list.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { products } from '../product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+  selectedCategory = '';
+    filteredProducts = this.products;
+
+    onCategoryChange() {
+        if (this.selectedCategory) {
+            this.filteredProducts = this.products.filter(product => product.category === this.selectedCategory);
+        } else {
+            this.filteredProducts = this.products;
+        }
+    }
+}
